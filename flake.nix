a52b6c5ee17998d8884b69c8582a6ac532b15265
Javier Feliz <javier0eduardo@hotmail.com>
2025-10-10 04:16:39 -0400

    Fix flake, add venv and makefile

--- flake.nix
+++ flake.nix
@@ -1,47 +1,37 @@
 {
-  description = "CartiChrono";
+  description = "CartiChrono Discord bot";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
   outputs =
     { self, nixpkgs }:
     let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-      };
-      pythonPackages = pkgs.python313Packages;
-      pyPkgs = with pythonPackages; [
-        python-dotenv
-        pip
-        nextcord
-        pynacl
-        cffi
-        pyogg
-        opuslib
-        gtts
-        pynacl
-
-      ];
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      py = pkgs.python313;
     in
     {
-      devShells.x86_64-linux = {
-        default = pkgs.mkShell {
-          buildInputs = [
-            pyPkgs
-            pkgs.ffmpeg
-            pkgs.libopus
-            pkgs.libsodium
-            pkgs.pkg-config
-            pkgs.alsa-lib
-            pkgs.pulseaudio
-            pkgs.pavucontrol
-          ];
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = [
+          pkgs.python313
+          pkgs.ffmpeg
+          pkgs.libopus
+          pkgs.libsodium
+          pkgs.pkg-config
+          pkgs.cacert
+          pkgs.openssl
+          pkgs.gnumake
+        ];
 
-          shellHook = ''
-            export PULSE_RUNTIME_PATH=/run/user/$(id -u)/pulse
-            export XDG_RUNTIME_DIR=/run/user/$(id -u)
-          '';
-        };
+        shellHook = ''
+          export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
+          export REQUESTS_CA_BUNDLE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
+          export LD_LIBRARY_PATH=${pkgs.libopus}/lib:${pkgs.libsodium}/lib:$LD_LIBRARY_PATH
+          export PULSE_RUNTIME_PATH=/run/user/$(id -u)/pulse
+          export XDG_RUNTIME_DIR=/run/user/$(id -u)
+          source venv/bin/activate
+        '';
       };
+
     };
 }
